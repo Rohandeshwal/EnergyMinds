8614c8f4b25194431dfc7d0b5a63ad8cac70fc8d
Rakeshmanral <63194001+Rakeshmanral@users.noreply.github.com>
2025-05-03 22:55:38 +0530

    Footer Done And Header In Pro

--- src/assessts/js/main.js
+++ src/assessts/js/main.js
@@ -1,20 +1,36 @@
 function loadHTML(id, file) {
-    fetch(file)
-      .then(response => {
-        if (!response.ok) throw new Error('Fetch error');
-        return response.text();
-      })
-      .then(data => {
-        document.getElementById(id).innerHTML = data;
-      })
-      .catch(error => {
-        console.error('Error loading', file, error);
-      });
+  fetch(file)
+    .then((response) => {
+      if (!response.ok) throw new Error("Fetch error");
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading", file, error);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadHTML("header", "/components/Header/header.html");
+  loadHTML("footer", "/components/Footer/footer.html");
+});
+
+var btn = document.getElementById("scrollTop");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
   }
-  
-  window.addEventListener('DOMContentLoaded', () => {
-    loadHTML('header', '/components/Header/header.html');
-    loadHTML('footer', '/components/Footer/footer.html');
+});
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
-  
-  
+});
